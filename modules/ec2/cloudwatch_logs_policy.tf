f94a2a12a7519cf09bc8a15bb477212cf6d81398
Steve <sjlewis84@yahoo.com>
2025-08-01 08:37:36 -0400

    Remove unnecessary files

---
+++ modules/ec2/cloudwatch_logs_policy.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_policy" "cloudwatch_logs" {
+  name        = "EC2CloudWatchLogsPolicy"
+  description = "Allows EC2 to send logs to CloudWatch Logs"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
